fc8cdbc789def4e6e8fb37471813cc4b0be4f30e
resursator <resursator@gmail.com>
2026-02-24 13:24:13 +0300

    debug uvicorn

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 REDIS_QUEUE = os.getenv("REDIS_QUEUE", "audio_tasks")
 
