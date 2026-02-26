716c5768ec4bf71a4cf8c8e24fa43078ec9213ed
resursator <resursator@gmail.com>
2026-02-26 21:17:10 +0300

    testing whisper-turbo

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch
 import pathlib
-from qwen_asr import Qwen3ASRModel
+import whisper
 
 base = pathlib.Path(__file__).parent / "examples/test"
 audio = []
@@ -18,25 +18,15 @@ audio.append(base / "download_14_opus_32k.ogg")
 audio.append(base / "download_14_wav.wav")
 
 def load_model():
-    return Qwen3ASRModel.from_pretrained(
-        "Qwen/Qwen3-ASR-1.7B",
-        dtype=torch.bfloat16,
-        device_map="cuda:0",
-        max_inference_batch_size=32,
-        max_new_tokens=256,
-    )
+    return whisper.load_model("turbo")
 
 model = load_model()
 
 for i in range(0,len(audio)):
     print("Trying " + str(audio[i].suffix) + ", the file is " + str(audio[i]))
     try:
-        results = model.transcribe(
-            audio=str(audio[i]),
-            language=None, # set "English" to force the language
-        )
-        # print(results[i].language)
-        print(results[0].text)
+        results = model.transcribe(str(audio[i]))
+        print(results["text"])
     except Exception:
         print("Failed extension " + str(audio[i].suffix))
         del model
